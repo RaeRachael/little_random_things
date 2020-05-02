6fda28ea7903d12979ee6893e8e30e802b616f0a
Rachael Ewins <rachaelewins@hotmail.com>
2020-05-02 09:12:43 +0000

    calculator passes complete spec

--- TDD_playground/calculator/calculator.rb
+++ TDD_playground/calculator/calculator.rb
@@ -3,20 +3,31 @@ class Calculator
     def operation(number1, operation, number2)
         case operation
         when "+"
-            return number1.to_i + number2.to_i
+            return number1.to_f + number2.to_f
         when "-"
-            return number1.to_i - number2.to_i 
+            return number1.to_f - number2.to_f 
         when "*"
-            return number1.to_i * number2.to_i
+            return number1.to_f * number2.to_f
         when "/"
-            return number1.to_i / number2.to_i           
+            return number1.to_f / number2.to_f           
         end
     end
     
     def evaluate(input)
         num_op = input.split(" ")
-        ans = num_op[0].to_i if num_op.length == 1
-        ans = operation(num_op[0],num_op[1],num_op[2]) if num_op.length == 3
-        ans
+        until num_op.length ==1
+            num_op.each_with_index do |op, index|
+                next if index%2 == 0
+                if num_op.count("*") > 0 || num_op.count("/") > 0
+                    next if op != "*" && op !="/"
+                end 
+                num_op[index - 1] = operation(num_op[index - 1], op, num_op[index + 1])
+                num_op.delete_at(index)
+                num_op.delete_at(index)
+                puts "#{num_op}"
+                break
+            end
+        end
+        num_op[0].to_f
     end
 end
--- TDD_playground/calculator/spec/calculator_spec.rb
+++ TDD_playground/calculator/spec/calculator_spec.rb
@@ -11,12 +11,15 @@ require_relative '../calculator'
 describe Calculator do
     calc = Calculator.new
     
+    describe "return a float number"do
     ["2","3","4","1","5","764","972394"].each do |num|
         it "should works for single number and no operator input" do
             expect(calc.evaluate(num)).to eq(num.to_i)
         end
     end
+    end
     
+    describe "can cope with one operator" do
     it "should use + operator correctly" do
         expect(calc.evaluate("1 + 3")).to eq(4)
     end
@@ -30,6 +33,57 @@ describe Calculator do
     end
     
     it "should use / operation correctly"do
-        expect(calc.evaluate("6 / 2")).to eq(3)
+        expect(calc.evaluate("5 / 2")).to eq(2.5)
+    end
+    end
+    
+    describe "can cope with two of the same operator" do
+    it "should use +, + correctly" do
+        expect(calc.evaluate("1 + 2 + 4")).to eq(7)
+    end
+    it "should use -, - correctly" do
+        expect(calc.evaluate("9 - 7 - 1")).to eq(1)        
+    end
+    it "should use *, * correctly" do
+        expect(calc.evaluate("2 * 3 * 2")).to eq(12)
+    end
+    it "should use /, / correctly" do
+        expect(calc.evaluate("12 / 3 / 2")).to eq(2)
+    end 
+    end
+    
+    describe "follows * and / priority convention" do
+    it "should do * operations before +" do
+        expect(calc.evaluate("2 * 4 + 3")).to eq(11)
+        
+        expect(calc.evaluate("2 + 4 * 3")).to eq(14)
+    end
+    it "should do / operations before +" do
+        expect(calc.evaluate("2 / 4 + 3")).to eq(3.5)
+        
+        expect(calc.evaluate("2 + 4 / 8")).to eq(2.5)
+    end
+    end
+    describe "should cope with both * and / operators" do
+        it "should return 4 for '4 * 3 / 3' input" do
+            expect(calc.evaluate("4 * 3 / 3")).to eq(4)
+        end
+        it "should return 4 for '3 * 4 / 3' input" do
+            expect(calc.evaluate("3 * 4 / 3")).to eq(4)
+        end       
+        it "should return 6 for '6 / 3 * 3' input" do
+            expect(calc.evaluate("6 / 3 * 3")).to eq(6)
+        end
+    end 
+    describe "should work with mix of all operators" do
+        it "should return 16 for '3 * 8 - 16 / 2' input" do
+            expect(calc.evaluate("3 * 8 - 16 / 4")).to eq(20)
+        end
+        it "should return -2 for '3 * 4 / 3 - 6 / 3 * 3' input" do
+            expect(calc.evaluate("3 * 4 / 3 - 6 / 3 * 3")).to eq(-2)
+        end
+        it "should return 8 for '2 + 3 * 4 / 3 - 6 / 3 * 3 + 8' input" do
+            expect(calc.evaluate("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8")).to eq(8)
+        end
     end
 end
